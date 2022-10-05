1e2bdd7e700b13af2cd624009787e25d631d8ef5
Adam Stirtan <adamstirtan@gmail.com>
2022-10-05 08:26:58 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <section>
+            <h1>Dashboard</h1>
+            <div class="tiles" data-title="Apps">
+                <div class="tile">
+
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+            </div>
+        </section>
+        <section>
+            <h1>Dashboard</h1>
+            <div class="tiles" data-title="Apps">
+                <div class="tile">
+
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+                <div class="tile">
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
